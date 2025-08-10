7decadc88463370e0f06dce26df8da9b8b8e6b52
Mikiyas Dereje <mikilegendpro@gmail.com>
2025-08-10 03:34:24 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    headers: {
+      "Cross-Origin-Opener-Policy": "same-origin",
+      "Cross-Origin-Embedder-Policy": "require-corp",
+    },
+  },
+  optimizeDeps: {
+    include: ["react-pdf", "pdfjs-dist"],
+  },
 });
